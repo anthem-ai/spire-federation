a5b39cfa867b2430b51fc8743a9d26e20402664a
Adam A <adam.amridin@anthem.com>
2021-04-23 14:50:39 -0600

    update script

--- scripts/trust-anthem-trust-bundle.sh
+++ scripts/trust-anthem-trust-bundle.sh
@@ -8,7 +8,7 @@ docker exec -it "${BASEDIR}_server_1" apk update
 docker exec -it "${BASEDIR}_server_1" apk add curl
 
 # grab trust bundle from anthem
-docker exec -it "${BASEDIR}_server_1" /bin/sh -c "curl -k https://spire-federation.staging.thos-hos-prod.anthemai.io:8443 > /tmp/test.json"
+docker exec -it "${BASEDIR}_server_1" /bin/sh -c "curl -k https://thos-hos-spire-federated-elb-1374501979.us-east-1.elb.amazonaws.com > /tmp/test.json"
 
 # add the trust bundle we downloaded to our certificate store
 docker exec -it "${BASEDIR}_server_1" ./bin/spire-server bundle set -format spiffe -id spiffe://hos-prod.k8s.anthemai -path /tmp/test.json
